18cdb7e259d5965652c5c31d82e454dc8e68291f
LY1177 <lpgamingg83@gmail.com>
2026-02-22 15:21:40 +0200

    Simulations added in handbook

--- public/handbook.js
+++ public/handbook.js
@@ -1,15 +1,15 @@
-document.querySelectorAll(".toggle").forEach(btn => {
+document.querySelectorAll(".handbook-card .toggle").forEach(btn => {
   btn.addEventListener("click", () => {
-    const content = btn.nextElementSibling;
+    const card = btn.closest(".handbook-card");
+    const content = card.querySelector(".handbook-content");
+
     content.classList.toggle("hidden");
-    btn.textContent = content.classList.contains("hidden")
-      ? "Покажи"
-      : "Скрий";
+    btn.textContent = content.classList.contains("hidden") ? "Покажи" : "Скрий";
   });
 });
 
 document.addEventListener("click", e => {
-  if(!e.target.classList.contains("zoomable")) return;
+  if (!e.target.classList.contains("zoomable")) return;
 
   const overlay = document.createElement("div");
   overlay.className = "image-overlay";
